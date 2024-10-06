a0c551a95d02fc07842bebb8726b8c27f551e53f
ShevelevMark <shevelev.mm@yandex.ru>
2024-10-06 16:02:33 +0300

    Созданы структуры и функции для хранения визуальной информации о моделируемом поле

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <view.h>
+
 
 int main() {
     initscr();
@@ -7,9 +10,32 @@ int main() {
     noecho();
     nodelay(stdscr,TRUE);
 
-    printw("Ncurses run!\n");
-    while(ERR == getch());
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
 
+    unsigned row_size = 20, col_size = 20;
+    int errcode;
+    pmk_view_field_t field = pmk_make_view_field(row_size, col_size, &errcode);
+    if (0 != errcode) {
+        printw("Can't allocate view field\nApplication terminated\n");
+        goto END;
+    }
+
+    for (unsigned row_idx = 0u; row_idx != row_size; ++row_idx)
+        for (unsigned col_idx = 0u; col_idx != col_size; ++col_idx)
+            field.cells[row_idx][col_idx] = (pmk_view_cell_t){1, '.'};
+
+    for (unsigned row_idx = 0u; row_idx != row_size; ++row_idx) {
+        for (unsigned col_idx = 0u; col_idx != col_size; ++col_idx) {
+            attron(COLOR_PAIR(1));
+            printw("%c", field.cells[row_idx][col_idx].glyph);
+            printw(" ");
+        }
+        printw("\n");
+    }
+END:
+    while(ERR == getch());
+    pmk_delete_view_field(&field);
     endwin();
+
     return 0;
 }
---
+++ src/view.c
@@ -0,0 +1,25 @@
+#include <view.h>
+#include <stdlib.h>
+
+pmk_view_field_t pmk_make_view_field(unsigned row_size, unsigned col_size, int *errcode) {
+    void *memory = malloc(row_size * sizeof(pmk_view_cell_t*) + row_size * col_size * sizeof(pmk_view_cell_t));
+    if (NULL == memory) {
+        *errcode = 1;
+        return (pmk_view_field_t){0u, 0u, NULL};
+    }
+
+    pmk_view_cell_t **cells_index = (pmk_view_cell_t**)memory;
+    pmk_view_cell_t *cells_memory = (pmk_view_cell_t*)((unsigned char *)memory + row_size * sizeof(pmk_view_cell_t*));
+    for (unsigned row_idx = 0u; row_idx != row_size; ++row_idx)
+        cells_index[row_idx] = cells_memory + row_idx * col_size;
+
+    *errcode = 0;
+    return (pmk_view_field_t){row_size, col_size, cells_index};
+}
+
+void pmk_delete_view_field(pmk_view_field_t *field) {
+    free((void*)(field->cells));
+    field->cells = NULL;
+    field->row_size = 0u;
+    field->col_size = 0u;
+}
