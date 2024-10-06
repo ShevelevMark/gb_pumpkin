78b97e4e032eb2389a632ff075f5d3e61723b802
ShevelevMark <shevelev.mm@yandex.ru>
2024-10-06 20:12:41 +0300

    Добавлены функции выделения и удаления памяти под наборы тыкв

--- inc/pumpkin_model.h
+++ inc/pumpkin_model.h
@@ -17,4 +17,32 @@ typedef pmk_pumpkin {
     pmk_pumpkin_state_t state; /**< состояние тыквы */
 } pmk_pumpkin_t;
 
+/**
+ * @brief Структура набора тыкв.
+ * Позволяет создать массив тыкв, сохраняющий свой размер.
+ */
+typedef struct pmk_garden {
+    unsigned size;
+    pmk_pumpkin_t *pumpkins;
+} pmk_garden_t;
+
+/**
+ * @brief Функция создания набора тыкв.
+ * Функция выделяет массив под набор тыкв.
+ * Если память выделить не удалось, то @param errcode перезаписывается в 1.
+ * Поле size перезаписывается в 0, поле pumpkins перезаписывается в NULL. 
+ * В случае успеха @param errcode перезаписывается в 0.
+ * @param size    - количество тыкв 
+ * @param errcode - указатель на код возврата
+ * @return pmk_garden_t 
+ */
+pmk_garden_t pmk_make_garden(unsigned size, int *errcode);
+
+/**
+ * @brief Функция удаления набора тыкв. 
+ * После удаления поле size перезаписывается в 0,
+ * поле pumpkins перезаписывается в NULL.
+ * @param garden - указатель на структуру набора тыкв
+ */
+void pmk_delete_garden(pmk_garden_t *garden);
 #endif
---
+++ src/pumpkin_model.c
@@ -0,0 +1,17 @@
+#include <pumpkin_model.h>
+
+pmk_garden_t pmk_make_garden(unsigned size, int *errcode) {
+    pmk_pumpkin_t *pumpkins = (pmk_pumpkin_t*)malloc(size * sizeof(pmk_pumpkin_t));
+    if (NULL == pumpkins) {
+        *errcode = 1;
+        return (pmk_garden_t){0u, NULL};
+    }
+    *errcode = 0;
+    return (pmk_garden_t){size, pumpkins};
+}
+
+void pmk_delete_garden(pmk_garden_t *garden) {
+    free(garden->pumpkins);
+    garden->size = 0u;
+    garden->pumpkins = NULL;
+}
