75507e1619557664a6c1b2f209f4d18d36e362ae
ShevelevMark <shevelev.mm@yandex.ru>
2024-10-07 19:23:58 +0300

    Добавлены комментарии

--- inc/garden_model.h
+++ inc/garden_model.h
@@ -1,15 +1,43 @@
 #ifndef PMK_GARDEN_MODEL_H
 #define PMK_GARDEN_MODEL_H
 
+/**
+ * @brief Перечисление состояний ячейки на сельскохозяйственном поле.
+ * PMK_ROAD   - дорога для дрона
+ * PMK_EMPTY  - пустая грядка
+ * PMK_UNRIPE - незрелая тыква на грядке
+ * PMK_RIPE   - зрелая тыква на грядке
+ */
 typedef enum pmk_garden_cell {PMK_ROAD = 0, PMK_EMPTY, PMK_UNRIPE, PMK_RIPE} pmk_garden_cell_t;
 
+/**
+ * @brief Структура хранит данные о ячейках и размерах сельскохозяйственного поля.
+ */
 typedef struct pmk_garden {
-    unsigned row_size, col_size;
-    pmk_garden_cell_t **cells;
+    unsigned row_size, col_size; /**< размеры поля: число рядов, число столбцов */
+    pmk_garden_cell_t **cells;   /**< указатель на массив ячеек */
 } pmk_garden_t;
 
+/**
+ * @brief Функция создания нового поля с тыквами.
+ * Функция создаёт новое поле с тыквами заданных размеров.
+ * Функция самостоятельно выделяет память под поле и индексный массив (массив указателей для удобного обращения к ячейкам).
+ * Если размеры @param row_size или @param col_size нулевы - поведение функции не определено.
+ * Если выделить память не удалось, то @param errcode будет перезаписано в 1.
+ * При успехе @param errcode будет перезаписано в 0.
+ * @param row_size - количество рядов
+ * @param col_size - количество стобцов
+ * @param errcode  - указатель на целое число, которое используется для кода возврата
+ * @return pmk_garden_t 
+ */
 pmk_garden_t pmk_make_garden(unsigned row_size, unsigned col_size, int *errcode);
 
+/**
+ * @brief Функция удаления памяти ячеек поля с тыквами.
+ * Функция удаляет память под поле с тыквами и записывает в структуру {0u, 0u, NULL}.
+ * @param garden - указатель на структуру поля, для которой необходимо освободить память.
+ * @warning переданная структура @param garden должна быть инициализированна функцией pmk_make_garden!
+ */
 void pmk_delete_garden(pmk_garden_t *garden);
 
 #endif
--- inc/garden_view.h
+++ inc/garden_view.h
@@ -4,10 +4,19 @@
 #include <view.h>
 #include <garden_model.h>
 
+/**
+ * @brief Структура для хранения данных об отображении поля с тыквами. 
+ */
 typedef struct pmk_garden_view {
     pmk_view_cell_t views[4]; /**< массив ячеек с данными для вывода, в котором каждый индекс соответствует значению pmk_garden_cell_state_t */
 } pmk_garden_view_t;
 
+/**
+ * @brief Функция переноса данных о поле с тыквами на поле для отображения в консоль. 
+ * @param field       - указатель на структуру поля для отображения 
+ * @param garden      - структура поля с тыквами
+ * @param garden_view - структура информации об отображении поля с тыквами
+ */
 void pmk_put_garden(pmk_view_field_t *field, pmk_garden_t garden, pmk_garden_view_t garden_view);
 
 #endif
