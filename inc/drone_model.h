0077c825c049256a8f101058f7d55e6e93515493
ShevelevMark <shevelev.mm@yandex.ru>
2024-10-14 17:31:17 +0300

    Изменена структура drone: добавлена возможность установить указатель на функцию движения и убрано направление движения

--- inc/drone_model.h
+++ inc/drone_model.h
@@ -12,13 +12,27 @@ typedef struct pmk_cart {
 } pmk_cart_t;
 
 /**
+ * @brief Предварительная декларация структуры дрона, чтобы мы могли объявить тип указателя на функцию работы дрона
+ */
+struct pmk_drone;
+
+/**
+ * @brief Объявляем тип указателя на функцию, которая будет двигать дрона.
+ * С каждым дроном связана некоторая функция, которая получает на вход 
+ * указатель на структуру дрона и некоторый произвольный контекст.
+ * Функция вызывается, чтобы дрон производил работу.
+ */
+typedef void (*pmk_drone_advance_t)(struct pmk_drone *, void *context);
+
+/**
  * @brief Структура моделирует дрон с тележками.  
  */
 typedef struct pmk_drone {
-    unsigned row_pos, col_pos; /**< положение дрона */
-    int row_dir, col_dir;      /**< скорость дрона, направление перемещения дрона */
-    unsigned carts_size;       /**< количество тележек, прицепленных к дрону */
-    pmk_cart_t *carts;         /**< указатель на массив тележек */
+    unsigned row_pos, col_pos;    /**< положение дрона */
+    unsigned carts_size;          /**< количество тележек, прицепленных к дрону */
+    pmk_cart_t *carts;            /**< указатель на массив тележек */
+    pmk_drone_advance_t advance;  /**< указатель на функцию работы дрона */
+    void *advance_context;        /**< дополнительное окружение для функции работы дрона */
 } pmk_drone_t;
 
 /**
@@ -31,13 +45,15 @@ typedef struct pmk_drone {
  * Если выделить память не удаётся или количество тележек равно нулю,
  * поле carts получает значение NULL.
  * 
- * @param row_pos     - ряд, в котором находится дрон 
- * @param col_pos     - колонка, в котором находится дрон
- * @param carts_size  - количество тележек
- * @param errcode     - указатель на целое число, в которое записывается код ошибки
+ * @param row_pos                  - ряд, в котором находится дрон 
+ * @param col_pos                  - колонка, в котором находится дрон
+ * @param carts_size               - количество тележек
+ * @param advance_strategy         - указатель на функцию работы дрона, может быть NULL
+ * @param advance_strategy_context - указатель на контекст функции работы дрона, может быть NULL
+ * @param errcode                  - указатель на целое число, в которое записывается код ошибки
  * @return pmk_drone_t структура дрона 
  */
-pmk_drone_t pmk_make_drone(unsigned row_pos, unsigned col_pos, unsigned carts_size, int *errcode);
+pmk_drone_t pmk_make_drone(unsigned row_pos, unsigned col_pos, unsigned carts_size, pmk_drone_advance_t advance_strategy, void *advance_strategy_context, int *errcode);
 
 /**
  * @brief Функция удаления памяти, выделенной под тележки.
